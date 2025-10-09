ff407ca8ba09bdd083cda0cc9ebb3616e8dea502
PaoloMar <PaoloMar_@hotmail.com>
2025-10-09 15:29:59 -0600

    Añadimos middleware a los endpoint de usuario

--- ApiRestReto/routes/usuarios.js
+++ ApiRestReto/routes/usuarios.js
@@ -1,14 +1,16 @@
 import express from 'express'
 const router = express.Router()
 
+import { middleware } from '../middleware/jwt.middleware.js'
+
 import { getUsers, getUser, disableUser, insertUsers, recoverUser } from "../controllers/usuarios.controller.js"
 
 
-router.get("/", getUsers)
-router.get("/:id", getUser)
-router.post("/insertar", insertUsers)
-router.patch("/desabilitar", disableUser)
-router.patch("/recuperar", recoverUser)
+router.get("/", middleware, getUsers)
+router.get("/:id", middleware, getUser)
+router.post("/insertar", middleware, insertUsers)
+router.patch("/desabilitar", middleware, disableUser)
+router.patch("/recuperar", middleware, recoverUser)
 
 
 
